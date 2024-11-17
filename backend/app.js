ae4d867b7c75fc09dc47d4b5ca586cc0827f0e22
HemanthDL <hemanthdl2907@gmail.com>
2024-11-18 05:21:10 +0530

    Uploaded🚀

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ conn()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cors({
-    origin: 'url-shortener-frontend-five.vercel.app', 
+    origin: 'https://url-shortener-frontend-five.vercel.app', 
     credentials: true 
 }));
 app.use(cookie_parser())
